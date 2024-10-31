e044c4f756f226543f124ea858a4cfda6bbf774d
Ahmed-Labs <ahmedelhagjr@gmail.com>
2024-10-30 22:29:57 -0400

    add response body and invalid api version error handling

---
+++ app/responses.go
@@ -0,0 +1,8 @@
+package main
+
+type ErrorCode int16
+
+const (
+	NONE                ErrorCode = 0
+	UNSUPPORTED_VERSION ErrorCode = 35
+)
--- app/server.go
+++ app/server.go
@@ -9,14 +9,18 @@ import (
 )
 
 type ResponseHeader struct {
-	correlationID uint32
+	correlationID int32
+}
+
+type ResponseBody struct {
+	errorCode ErrorCode
 }
 
 type RequestHeader struct {
-	size int
-	requestApiKey     uint16
-	requestApiVersion uint16
-	correlationID     uint32
+	size              int
+	requestApiKey     int16
+	requestApiVersion int16
+	correlationID     int32
 	clientID          string
 }
 
@@ -30,25 +34,31 @@ func checkError(err error) {
 	}
 }
 
+func (h RequestHeader) validate() ErrorCode {
+	if !(int(h.requestApiVersion) >= 0 && int(h.requestApiVersion) <= 2) {
+		return UNSUPPORTED_VERSION
+	}
+	return NONE
+}
+
 func (h *RequestHeader) deserialize(header []byte) {
-	h.requestApiKey = binary.BigEndian.Uint16(header[:2])
-	h.requestApiVersion = binary.BigEndian.Uint16(header[2:4])
-	h.correlationID = binary.BigEndian.Uint32(header[4:8])
+	h.requestApiKey = int16(binary.BigEndian.Uint16(header[:2]))
+	h.requestApiVersion = int16(binary.BigEndian.Uint16(header[2:4]))
+	h.correlationID = int32(binary.BigEndian.Uint32(header[4:8]))
 	h.clientID = string(header[8:])
 }
 
-func getRequestMessage(conn net.Conn) RequestMessage{
+func getRequestMessage(conn net.Conn) RequestMessage {
 	sizeBytes := make([]byte, 4)
 	_, err := io.ReadFull(conn, sizeBytes)
 	checkError(err)
 
 	size := int(binary.BigEndian.Uint32(sizeBytes))
-
 	data := make([]byte, size)
 	_, err = io.ReadFull(conn, data)
 	checkError(err)
 
-	header := RequestHeader{ size: size }
+	header := RequestHeader{size: size}
 	header.deserialize(data)
 
 	return RequestMessage{
@@ -62,19 +72,34 @@ func sendResponse(conn net.Conn, responseMessage []byte) {
 	checkError(err)
 }
 
+func (rb ResponseBody) serialize() []byte {
+	body := []byte{}
+
+	if rb.errorCode != NONE {
+		errCodeBytes := make([]byte, 2)
+		binary.BigEndian.PutUint16(errCodeBytes, uint16(rb.errorCode))
+		return errCodeBytes
+	}
+
+	return body
+}
+
 func (rs ResponseHeader) serialize() []byte {
 	correlationID := make([]byte, 4)
-	binary.BigEndian.PutUint32(correlationID, rs.correlationID)
+	binary.BigEndian.PutUint32(correlationID, uint32(rs.correlationID))
 	return correlationID
 }
 
-func buildResponseMessage(header ResponseHeader) []byte {
+func buildResponseMessage(header ResponseHeader, body ResponseBody) []byte {
 	serializedHeader := header.serialize()
-	messageSize := len(serializedHeader)
+	serializedBody := body.serialize()
+	messageSize := len(serializedHeader) + len(serializedBody)
 
 	message := make([]byte, 4+messageSize) // messageSize itself is 4 bytes
+
 	binary.BigEndian.PutUint32(message, uint32(messageSize))
 	copy(message[4:], serializedHeader)
+	copy(message[4+len(serializedHeader):], serializedBody)
 
 	return message
 }
@@ -93,10 +118,16 @@ func main() {
 	defer conn.Close()
 
 	requestMessage := getRequestMessage(conn)
-	
+
+	responseBody := ResponseBody{}
+	if err := requestMessage.header.validate(); err != NONE {
+		responseBody.errorCode = UNSUPPORTED_VERSION
+	}
+
 	responseHeader := ResponseHeader{
 		correlationID: requestMessage.header.correlationID,
 	}
-	responseMessage := buildResponseMessage(responseHeader)
+
+	responseMessage := buildResponseMessage(responseHeader, responseBody)
 	sendResponse(conn, responseMessage)
 }
